764f4bb292ee6836aed727d0cbf11f636c5c515d
IvanDamNation <ivan.suvalkin@gmail.com>
2023-04-04 11:32:50 +0300

    Add first test form in admin page (works)

--- accounts_app/admin.py
+++ accounts_app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from accounts_app.models import FactoryManager, ServiceManager, Customer
+
+# class NewsAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'author', 'title', 'category', 'created_at', 'updated_at', 'is_published')
+#     list_display_links = ('id', 'title')
+#     search_fields = ('title', 'author', 'content')
+#     list_editable = ('is_published', )
+#     list_filter = ('is_published', 'category')
+
+# FactoryManager
+# ServiceManager
+# Customer
+
+
+# admin.site.register(FactoryManager, FactoryManagerAdmin)
+# admin.site.register(ServiceManager, ServiceManagerAdmin)
+# admin.site.register(Customer, CustomerAdmin)
--- machine_service_app/admin.py
+++ machine_service_app/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from machine_service_app.models import Machine, Maintenance, Complaint
+
+
+# class NewsAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'author', 'title', 'category', 'created_at', 'updated_at', 'is_published')
+#     list_display_links = ('id', 'title')
+#     search_fields = ('title', 'author', 'content')
+#     list_editable = ('is_published', )
+#     list_filter = ('is_published', 'category')
+
+
+class MachineAdmin(admin.ModelAdmin):
+    list_display = ('factory_number', 'model', 'supply_contract', 'client', )
+    list_display_links = ('factory_number', 'client', )
+    search_fields = ('factory_number', 'supply_contract', 'client', )
+    list_filter = ('model', 'client', )
+
+# Maintenance
+# Complaint
+
+
+admin.site.register(Machine, MachineAdmin)
+# admin.site.register(Maintenance, MaintenanceAdmin)
+# admin.site.register(Complaint, ComplaintAdmin)
